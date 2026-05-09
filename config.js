2a7a79154e5eb5c36a99ec7d5ef99376ab7a73c3
Mench19 <armenchik19.petrosyan@gmail.com>
2026-05-09 20:06:45 +0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// ===== НАСТРОЙКИ =====
-// Замени на свои значения из Supabase Project Settings → API
-
-const SUPABASE_URL = 'https://xvvacjogqpkalxlnhfvs.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2dmFjam9ncXBrYWx4bG5oZnZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNTc3OTksImV4cCI6MjA5MzgzMzc5OX0.Y5aw2i4xICySzFNbnOxOpIJtAfpkj6rnDHleRIM5X-k';
